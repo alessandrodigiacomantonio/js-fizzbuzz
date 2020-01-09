ee6934c839d5074c1bcc1d4656609bd31c72f035
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-09 16:54:09 +0100

    Js: risoluzione con migliore prestazione
    facendo vari test, sono giunto alla conclusione che per questo specifico esercizio, non si può sapere quale metodo sia più veloce, in quanto i tempi di esecuzione di ogni ciclo danno risultati con una deviazione standard molto alta. Detto questo, visto la semplicità delle operazioni, la soluzione migliore è puntare alla metodologia che risulta più chiara da interpretare, in quanto la differenza di velocità di esecuzione è decisamente trascurabile.

--- js/style.js
+++ js/style.js
@@ -1,37 +1,36 @@
+// console.time('Tempo impiegato METODO UNO');
+// for (var i = 1; i <= 100; i++) {
+//   if (i%15 == 0) {
+//     console.log('FizzBuzz');
+//   }
+//   if ((i%15 != 0) && (i%3 == 0)) {
+//     console.log('Buzz');
+//   }
+//   if ((i%15 != 0) && (i%5 == 0)) {
+//     console.log('Fizz');
+//   }
+//   if ((i%15 != 0) && (i%5 != 0) && (i%3 != 0)) {
+//     console.log(i);
+//   }
+// }
+// console.timeEnd('Tempo impiegato METODO UNO');
+
+// console.time('Tempo impiegato METODO DUE');
+// for (var i = 1; i <= 100; i++) {
+//   if (i%3 == 0) {
+//     if (i%15 == 0) {
+//       console.log('FizzBuzz');
+//     }  else console.log('Fizz');
+//   } else if (i%5 == 0) {
+//           if(i%15 == 0) {
+//             console.log('FizzBuzz');
+//           } else console.log('Buzz');
+//   } else console.log(i);
+// }
+// console.timeEnd('Tempo impiegato METODO DUE');
+//
+console.time('Tempo impiegato METODO TRE');
 for (var i = 1; i <= 100; i++) {
-
-  //------
-  //METODO UNO
-  //------
-  // if (i%15 == 0) {
-  //   console.log('FizzBuzz');
-  // }
-  // if ((i%15 != 0) && (i%5 == 0)) {
-  //   console.log('Buzz');
-  // }
-  // if ((i%15 != 0) && (i%5 != 0) && (i%3 == 0)) {
-  //   console.log('Fizz');
-  // }
-  // if ((i%15 != 0) && (i%5 != 0) && (i%3 != 0)) {
-  //   console.log(i);
-  // }
-
-//------
-//METODO DUE
-// ------
-  // if (i%3 == 0) {
-  //   if (i%15 == 0) {
-  //     console.log('FizzBuzz');
-  //   }  else console.log('Fizz');
-  // } else if (i%5 == 0) {
-  //         if(i%15 == 0) {
-  //           console.log('FizzBuzz');
-  //         } else console.log('Buzz');
-  // } else console.log(i);
-
-//------
-//METODO TRE
-//------
   if (i%5 != 0 && i%3 != 0 ) {
     console.log(i);
   } else if (i%15 == 0) {
@@ -42,3 +41,4 @@ for (var i = 1; i <= 100; i++) {
     console.log('Buzz');
   }
 }
+console.timeEnd('Tempo impiegato METODO TRE');

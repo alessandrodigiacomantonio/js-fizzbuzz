8f0b7e85b4ec71e4b93022414a329bde27e7ef0f
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-09 14:23:20 +0100

    Js: tre metodi per risolvere, quale ha prestazioni migliori?

--- js/style.js
+++ js/style.js
@@ -1,14 +1,44 @@
 for (var i = 1; i <= 100; i++) {
-  if (i%15 == 0) {
-    console.log('FizzBuzz');
-  }
-  if ((i%15 != 0) && (i%5 == 0)) {
-    console.log('Buzz');
-  }
-  if ((i%15 != 0) && (i%5 != 0) && (i%3 == 0)) {
-    console.log('Fizz');
-  }
-  if ((i%15 != 0) && (i%5 != 0) && (i%3 != 0)) {
+
+  //------
+  //METODO UNO
+  //------
+  // if (i%15 == 0) {
+  //   console.log('FizzBuzz');
+  // }
+  // if ((i%15 != 0) && (i%5 == 0)) {
+  //   console.log('Buzz');
+  // }
+  // if ((i%15 != 0) && (i%5 != 0) && (i%3 == 0)) {
+  //   console.log('Fizz');
+  // }
+  // if ((i%15 != 0) && (i%5 != 0) && (i%3 != 0)) {
+  //   console.log(i);
+  // }
+
+//------
+//METODO DUE
+// ------
+  // if (i%3 == 0) {
+  //   if (i%15 == 0) {
+  //     console.log('FizzBuzz');
+  //   }  else console.log('Fizz');
+  // } else if (i%5 == 0) {
+  //         if(i%15 == 0) {
+  //           console.log('FizzBuzz');
+  //         } else console.log('Buzz');
+  // } else console.log(i);
+
+//------
+//METODO TRE
+//------
+  if (i%5 != 0 && i%3 != 0 ) {
     console.log(i);
+  } else if (i%15 == 0) {
+    console.log('FizzBuzz');
+  } else if (i%15 != 0 && i%3 == 0) {
+    console.log('Fizz');
+  } else if (i%15 != 0 && i%5 == 0) {
+    console.log('Buzz');
   }
 }

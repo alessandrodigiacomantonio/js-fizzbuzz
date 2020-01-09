5901393f3dc71c5f41f0cf0a140a00720d84b756
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-09 17:16:27 +0100

    js: stampa anche su documento html
    il tempo di esecuzione del ciclo è aumentato di 3-4 volte

--- js/style.js
+++ js/style.js
@@ -2,15 +2,19 @@
 // for (var i = 1; i <= 100; i++) {
 //   if (i%15 == 0) {
 //     console.log('FizzBuzz');
+//     document.getElementsByClassName('lista')[0].innerHTML += '<li>FizzBuzz</li>';
 //   }
 //   if ((i%15 != 0) && (i%3 == 0)) {
 //     console.log('Buzz');
+//     document.getElementsByClassName('lista')[0].innerHTML += '<li>Buzz</li>';
 //   }
 //   if ((i%15 != 0) && (i%5 == 0)) {
 //     console.log('Fizz');
+//     document.getElementsByClassName('lista')[0].innerHTML += '<li>Fizz</li>';
 //   }
 //   if ((i%15 != 0) && (i%5 != 0) && (i%3 != 0)) {
 //     console.log(i);
+//     document.getElementsByClassName('lista')[0].innerHTML += '<li>'+i+'</li>';
 //   }
 // }
 // console.timeEnd('Tempo impiegato METODO UNO');
@@ -20,25 +24,40 @@
 //   if (i%3 == 0) {
 //     if (i%15 == 0) {
 //       console.log('FizzBuzz');
-//     }  else console.log('Fizz');
+//       document.getElementsByClassName('lista')[0].innerHTML += '<li>FizzBuzz</li>';
+//     }  else {
+//         console.log('Fizz');
+//         document.getElementsByClassName('lista')[0].innerHTML += '<li>Fizz</li>';
+//       }
 //   } else if (i%5 == 0) {
 //           if(i%15 == 0) {
 //             console.log('FizzBuzz');
-//           } else console.log('Buzz');
-//   } else console.log(i);
+//             document.getElementsByClassName('lista')[0].innerHTML += '<li>FizzBuzz</li>';
+//           } else {
+//             console.log('Buzz');
+//             document.getElementsByClassName('lista')[0].innerHTML += '<li>Buzz</li>';
+//           }
+//   } else {
+//       console.log(i);
+//       document.getElementsByClassName('lista')[0].innerHTML += '<li>'+i+'</li>';
+//     }
 // }
 // console.timeEnd('Tempo impiegato METODO DUE');
-//
+
 console.time('Tempo impiegato METODO TRE');
 for (var i = 1; i <= 100; i++) {
   if (i%5 != 0 && i%3 != 0 ) {
     console.log(i);
+    document.getElementsByClassName('lista')[0].innerHTML += '<li>'+i+'</li>';
   } else if (i%15 == 0) {
     console.log('FizzBuzz');
+    document.getElementsByClassName('lista')[0].innerHTML += '<li>FizzBuzz</li>';
   } else if (i%15 != 0 && i%3 == 0) {
     console.log('Fizz');
+    document.getElementsByClassName('lista')[0].innerHTML += '<li>Fizz</li>';
   } else if (i%15 != 0 && i%5 == 0) {
     console.log('Buzz');
+    document.getElementsByClassName('lista')[0].innerHTML += '<li>Buzz</li>';
   }
 }
 console.timeEnd('Tempo impiegato METODO TRE');
